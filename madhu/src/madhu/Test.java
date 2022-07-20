86b389ff95d6554ab85e975bf2516cfdaa384b46
Guess <Guess@DESKTOP-2S2DGF7>
2022-07-20 13:33:42 +0530

    first commit

---
+++ madhu/src/madhu/Test.java
@@ -0,0 +1,11 @@
+package madhu;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.print("Y"+"O");
+		System.out.print('L'+'O');// TODO Auto-generated method stub
+
+	}
+
+}
